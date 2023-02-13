916a93ac0e5f42a29b30b01b979cd15d6d83f16d
陈磊 <gbdvjxu1257q@outlook.com>
2023-02-13 16:14:25 +0800

    考试接口

---
+++ aspnet-core/src/SuperAbp.Exam.Application/ExamManagement/ExamManagementApplicationAutoMapperProfile.cs
@@ -0,0 +1,24 @@
+﻿using SuperAbp.Exam.ExamManagement.Exams;
+using AutoMapper;
+
+namespace SuperAbp.Exam.ExamManagement
+{
+    /// <summary>
+    /// Mapper映射配置
+    /// </summary>
+    public class ExamManagementApplicationAutoMapperProfile : Profile
+    {
+        /// <summary>
+        /// .ctor
+        /// </summary>
+        public ExamManagementApplicationAutoMapperProfile()
+        {
+            #region 考试
+
+            CreateMap<Examing, ExamingListDto>();
+            CreateMap<Examing, ExamingDetailDto>();
+
+            #endregion 考试
+        }
+    }
+}
