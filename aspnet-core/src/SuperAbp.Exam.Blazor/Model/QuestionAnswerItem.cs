08ac3fb869fa18a0a96eee7d2a4c58495742e5ab
陈磊 <gbdvjxu1257q@outlook.com>
2023-07-17 17:46:46 +0800

    修改答案展示方式

---
+++ aspnet-core/src/SuperAbp.Exam.Blazor/Model/QuestionAnswerItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SuperAbp.Exam.Blazor.Model;
+
+public class QuestionAnswerItem
+{
+    public Guid QuestionId { get; set; }
+    public string AnswerId { get; set; }
+    public bool Right { get; set; }
+}
