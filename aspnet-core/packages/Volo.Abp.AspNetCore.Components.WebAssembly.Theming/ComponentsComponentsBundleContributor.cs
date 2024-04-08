8c4bfc2c00a5711b9b0d2934b3acf3bf365c4a79
陈磊 <gbdvjxu1257q@outlook.com>
2024-04-08 21:01:06 +0800

    add theming

---
+++ aspnet-core/packages/Volo.Abp.AspNetCore.Components.WebAssembly.Theming/ComponentsComponentsBundleContributor.cs
@@ -0,0 +1,17 @@
+﻿using Volo.Abp.Bundling;
+
+namespace Volo.Abp.AspNetCore.Components.WebAssembly.Theming;
+
+public class ComponentsComponentsBundleContributor : IBundleContributor
+{
+    public void AddScripts(BundleContext context)
+    {
+        context.Add("_content/Microsoft.AspNetCore.Components.WebAssembly.Authentication/AuthenticationService.js");
+        context.Add("_content/Volo.Abp.AspNetCore.Components.Web/libs/abp/js/abp.js");
+        context.Add("_content/Volo.Abp.AspNetCore.Components.Web/libs/abp/js/lang-utils.js");
+    }
+
+    public void AddStyles(BundleContext context)
+    {
+    }
+}
