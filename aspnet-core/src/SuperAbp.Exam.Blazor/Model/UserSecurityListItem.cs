28bfd9c42cedd264608f0f3236e2a81bd1d3b80d
snowchenlei <gbdvjxu1257q@outlook.com>
2025-04-01 17:36:29 +0800

    add info

---
+++ aspnet-core/src/SuperAbp.Exam.Blazor/Model/UserSecurityListItem.cs
@@ -0,0 +1,8 @@
+﻿namespace SuperAbp.Exam.Blazor.Model
+{
+    public class UserSecurityListItem
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
