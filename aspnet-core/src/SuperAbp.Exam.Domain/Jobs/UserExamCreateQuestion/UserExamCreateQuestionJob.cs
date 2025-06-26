95aef1cb2a674fc0422601d71ef1ab7a3d5fb9c4
snow <gbdvjxu1257q@outlook.com>
2025-06-26 17:16:28 +0800

    show progress

---
+++ aspnet-core/src/SuperAbp.Exam.Domain/Jobs/UserExamCreateQuestion/UserExamCreateQuestionJob.cs
@@ -0,0 +1,14 @@
+﻿using SuperAbp.Exam.ExamManagement.UserExams;
+using System.Threading.Tasks;
+using Volo.Abp.BackgroundJobs;
+using Volo.Abp.DependencyInjection;
+
+namespace SuperAbp.Exam.Jobs.UserExamCreateQuestion;
+
+public class UserExamCreateQuestionJob(IUserExamRepository userExamRepository, UserExamManager userExamManager) : AsyncBackgroundJob<UserExamCreateQuestionArgs>, ITransientDependency
+{
+    public override async Task ExecuteAsync(UserExamCreateQuestionArgs args)
+    {
+        await userExamManager.CreateQuestionsAsync(args.UserExamId);
+    }
+}
