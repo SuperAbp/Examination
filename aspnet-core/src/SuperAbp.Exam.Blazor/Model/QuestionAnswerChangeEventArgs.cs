e2003192c0d5eda4bfa539adc59b4dfd74ded230
snow <gbdvjxu1257q@outlook.com>
2025-08-06 16:52:17 +0800

    auto save

---
+++ aspnet-core/src/SuperAbp.Exam.Blazor/Model/QuestionAnswerChangeEventArgs.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SuperAbp.Exam.Blazor.Model;
+
+public class QuestionAnswerChangeEventArgs
+{
+    public Guid QuestionId { get; set; }
+    public string Answer { get; set; }
+}
