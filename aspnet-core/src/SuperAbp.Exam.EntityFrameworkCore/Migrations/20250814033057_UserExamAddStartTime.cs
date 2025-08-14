332a3c554076d917096bb8d43099086246e92364
snow <gbdvjxu1257q@outlook.com>
2025-08-14 14:08:24 +0800

    Countdown

---
+++ aspnet-core/src/SuperAbp.Exam.EntityFrameworkCore/Migrations/20250814033057_UserExamAddStartTime.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SuperAbp.Exam.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserExamAddStartTime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartTime",
+                table: "AppUserExam",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StartTime",
+                table: "AppUserExam");
+        }
+    }
+}
