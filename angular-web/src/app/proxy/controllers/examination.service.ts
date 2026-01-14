9519be6f571ac4da9d35bf9f3cc3e394b026ba4f
snow <gbdvjxu1257q@outlook.com>
2026-01-14 15:36:04 +0800

    fix search error

--- angular-web/src/app/proxy/controllers/examination.service.ts
+++ angular-web/src/app/proxy/controllers/examination.service.ts
@@ -1,7 +1,12 @@
 import { RestService, Rest } from '@abp/ng.core';
 import type { ListResultDto, PagedResultDto } from '@abp/ng.core';
 import { Injectable, inject } from '@angular/core';
-import type { ExamDetailDto, ExamListDto, ExamRankingDto, GetExamsInput } from '../exam-management/exams/models';
+import type {
+  ExamDetailDto,
+  ExamListDto,
+  ExamRankingDto,
+  GetExamsInput,
+} from '../exam-management/exams/models';
 
 @Injectable({
   providedIn: 'root',
@@ -9,29 +14,38 @@ import type { ExamDetailDto, ExamListDto, ExamRankingDto, GetExamsInput } from '
 export class ExaminationService {
   private restService = inject(RestService);
   apiName = 'Default';
-  
 
   get = (id: string, config?: Partial<Rest.Config>) =>
-    this.restService.request<any, ExamDetailDto>({
-      method: 'GET',
-      url: `/api/exams/${id}`,
-    },
-    { apiName: this.apiName,...config });
-  
+    this.restService.request<any, ExamDetailDto>(
+      {
+        method: 'GET',
+        url: `/api/exams/${id}`,
+      },
+      { apiName: this.apiName, ...config },
+    );
 
   getList = (input: GetExamsInput, config?: Partial<Rest.Config>) =>
-    this.restService.request<any, PagedResultDto<ExamListDto>>({
-      method: 'GET',
-      url: '/api/exams',
-      params: { name: input.name, sorting: input.sorting, skipCount: input.skipCount, maxResultCount: input.maxResultCount },
-    },
-    { apiName: this.apiName,...config });
-  
+    this.restService.request<any, PagedResultDto<ExamListDto>>(
+      {
+        method: 'GET',
+        url: '/api/exams',
+        params: {
+          status: input.status,
+          name: input.name,
+          sorting: input.sorting,
+          skipCount: input.skipCount,
+          maxResultCount: input.maxResultCount,
+        },
+      },
+      { apiName: this.apiName, ...config },
+    );
 
   getRankingList = (id: string, config?: Partial<Rest.Config>) =>
-    this.restService.request<any, ListResultDto<ExamRankingDto>>({
-      method: 'GET',
-      url: `/api/exams/${id}/ranking`,
-    },
-    { apiName: this.apiName,...config });
+    this.restService.request<any, ListResultDto<ExamRankingDto>>(
+      {
+        method: 'GET',
+        url: `/api/exams/${id}/ranking`,
+      },
+      { apiName: this.apiName, ...config },
+    );
 }
