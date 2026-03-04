35c2d9bce2598b3589d06d81700bc77a754cc001
snow <gbdvjxu1257q@outlook.com>
2026-03-04 14:59:25 +0800

    update abp@10.1

--- aspnet-core/src/SuperAbp.Exam.Admin.HttpApi.Host/ExamHttpApiHostModule.cs
+++ aspnet-core/src/SuperAbp.Exam.Admin.HttpApi.Host/ExamHttpApiHostModule.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi;
 using Rebus.Config;
 using StackExchange.Redis;
 using SuperAbp.Exam.EntityFrameworkCore;
